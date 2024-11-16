2637f5727492e360c61e0639316a364ccbeac279
TusKabina <ivanrogulj248@gmail.com>
2024-11-16 14:13:16 +0100

    removed unused files

--- Includes/Qt/WorkerThread.h
+++
@@ -1,9 +0,0 @@
-//#ifndef SFTP_CLIENT_WORKERTHREAD_H
-//#define SFTP_CLIENT_WORKERTHREAD_H
-//#include <QtCore/QThread>
-//#include "ThreadPool.h"
-//
-//
-//
-//
-//#endif // SFTP_CLIENT_WORKERTHREAD_H
