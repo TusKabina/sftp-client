5e3a330469da64d34684f1f007135bee5761ba48
TusKabina <ivanrogulj248@gmail.com>
2025-07-22 23:16:23 +0200

    modify methods to be inline and to log with logger

--- Includes/Utilities/MeasureHelper.h
+++ Includes/Utilities/MeasureHelper.h
@@ -5,13 +5,13 @@
 #include <chrono>
 #include <functional>
 #include <type_traits>
-
+#include "Utilities/Logger.h"
 
 namespace MeasureHelper {
 
     template <typename Func, typename... Args>
     std::enable_if_t<std::is_void_v<std::invoke_result_t<Func, Args...>>>
-    Time(Func&& func, Args&&... args) {
+    inline Time(Func&& func, Args&&... args) {
         auto start = std::chrono::high_resolution_clock::now();
 
         std::invoke(std::forward<Func>(func), std::forward<Args>(args)...);
@@ -19,10 +19,11 @@ namespace MeasureHelper {
         auto end = std::chrono::high_resolution_clock::now();
         std::chrono::duration<double, std::milli> duration = end - start;
         std::cout << "Execution time: " << duration.count() << " ms\n";
+        logger().debug() << "Execution time: " << duration.count() << " ms";
     }
 
     template <typename Func, typename... Args>
-    auto Time(Func&& func, Args&&... args) -> std::enable_if_t<!std::is_void_v<std::invoke_result_t<Func, Args...>>, std::invoke_result_t<Func, Args...>> {
+    inline auto Time(Func&& func, Args&&... args) -> std::enable_if_t<!std::is_void_v<std::invoke_result_t<Func, Args...>>, std::invoke_result_t<Func, Args...>> {
         auto start = std::chrono::high_resolution_clock::now();  
 
    
@@ -31,14 +32,19 @@ namespace MeasureHelper {
         auto end = std::chrono::high_resolution_clock::now();  
         std::chrono::duration<double, std::milli> duration = end - start;
         std::cout << "Execution time: " << duration.count() << " ms\n";
+		logger().debug() << "Execution time: " << duration.count() << " ms";
 
         return result;
     }
 
-    auto logDuration = [](const std::string& section, auto start, auto end) {
+    inline auto logDuration = [](const std::string& section, auto start, auto end) {
         std::cout << section << " took: "
             << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()
             << " ms" << std::endl;
+
+        logger().debug() << section << " took: "
+            << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()
+            << " ms";
     };
 }
 #endif // SFTP_CLIENT_MEASURE_HELPER_H
